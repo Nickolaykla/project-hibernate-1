146ef8d6d07a9c3a9bdba15de7797e48c1fd196e
nickolaykla <nickolaykla@gmail.com>
2025-12-09 19:06:21 +0300

    taskNine

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,17 +1,35 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import com.game.entity.Profession;
+import com.game.entity.Race;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.springframework.stereotype.Repository;
 
 import jakarta.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
+    private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
-
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+        sessionFactory = new Configuration()
+                            .setProperties(properties)
+                            .addAnnotatedClass(Player.class)
+                            .addAnnotatedClass(Profession.class)
+                            .addAnnotatedClass(Race.class)
+                            .buildSessionFactory();
     }
 
     @Override
