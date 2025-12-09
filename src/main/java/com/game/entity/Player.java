27e34c344e358725910436cdf319206ec690467b
nickolaykla <nickolaykla@gmail.com>
2025-12-09 18:35:14 +0300

    task9

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,33 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
-
+@Entity @Table(name = "player", schema = "rpg")
 public class Player {
+    @Id @GeneratedValue
     private Long id;
 
+    @Column(nullable = false, length = 12)
     private String name;
 
+    @Column(nullable = false, length = 30)
     private String title;
 
+    @Column(nullable = false)
     private Race race;
 
+    @Column(nullable = false)
     private Profession profession;
 
+    @Column(nullable = false)
     private Date birthday;
 
+    @Column(nullable = false)
     private Boolean banned;
 
+    @Column(nullable = false)
     private Integer level;
 
     public Player() {
--- src/main/java/com/game/entity/Profession.java
+++ src/main/java/com/game/entity/Profession.java
@@ -1,12 +1,23 @@
 package com.game.entity;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
 public enum Profession {
+    @Enumerated(EnumType.ORDINAL)
     WARRIOR,
+    @Enumerated(EnumType.ORDINAL)
     ROGUE,
+    @Enumerated(EnumType.ORDINAL)
     SORCERER,
+    @Enumerated(EnumType.ORDINAL)
     CLERIC,
+    @Enumerated(EnumType.ORDINAL)
     PALADIN,
+    @Enumerated(EnumType.ORDINAL)
     NAZGUL,
+    @Enumerated(EnumType.ORDINAL)
     WARLOCK,
+    @Enumerated(EnumType.ORDINAL)
     DRUID
 }
